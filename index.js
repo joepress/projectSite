26f3be32db8e8b77e4a4ed34798c779d40c124a5
unknown <jmpressley16@gmail.com>
2019-08-07 16:22:12 -0400

    finish place holders and starting mobile

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
  // menu to hide and come back
   function openNav(){
+		if(screen.width = '375'){
+			document.getElementById("navSidePanel").style.height = '15%';
+			document.getElementById("menuBtn").style.height = '0';
+			document.getElementById("menuBtn").style.padding = '0 0';
+			document.getElementById("menuOffSet").style.display = 'none';
+			document.getElementById("social-bar").style.top = '21.9%';
+		}
+		else{
+			console.log(screen.width);
 		document.getElementById("navSidePanel").style.width = '13%';
 		document.getElementById("menuBtn").style.width = '0';
 		document.getElementById("menuBtn").style.padding = '0 0';
 		document.getElementById("menuOffSet").style.display = 'none';
+		
+		}
 }
 
 	function closeNav(){
-		document.getElementById("menuBtn").style.width = '2.7%';
-		document.getElementById("menuBtn").style.padding = '10px 0px';
-		document.getElementById("menuOffSet").style.display = 'unset';
-		document.getElementById("navSidePanel").style.width ='0';
+		if(screen.width = '375'){
+			document.getElementById("menuBtn").style.height = '8.2%';
+			document.getElementById("menuOffSet").style.display = 'unset';
+			document.getElementById("navSidePanel").style.height ='0';
+			document.getElementById("social-bar").style.top = '12.3%';
+		}
+		else{
+			document.getElementById("menuBtn").style.width = '2.7%';
+			document.getElementById("menuBtn").style.padding = '10px 0px';
+			document.getElementById("menuOffSet").style.display = 'unset';
+			document.getElementById("navSidePanel").style.width ='0';
+		}
 }
 
   function conOpenNav(){
@@ -21,6 +40,20 @@
 }
 
 	function conCloseNav(){
+		document.getElementById("conMenuBtn").style.width = '3.2%';
+		document.getElementById("conMenuBtn").style.padding = '1.15% 0px';
+		document.getElementById("conMenuOffSet").style.display = 'unset';
+		document.getElementById("conNavSidePanel").style.width ='0';
+}
+
+
+  function mobileOpenNav(){
+		
+		
+}
+
+	function conCloseNav(){
+		if($('.'))
 		document.getElementById("conMenuBtn").style.width = '3.2%';
 		document.getElementById("conMenuBtn").style.padding = '1.15% 0px';
 		document.getElementById("conMenuOffSet").style.display = 'unset';
@@ -39,6 +72,22 @@ window.onscroll = function() {
     document.getElementById("menuBtn").classList.add('animated', 'zoomIn' ,'delay-0.3s');
   } else {
     document.getElementById("menuBtn").classList.add('animated', 'zoomOutUp' ,'delay-0.3s');
+    document.getElementById("social-bar").classList.add('animated', 'zoomOutRight' ,'delay-0.5s');
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("conMenuBtn").classList.remove('animated', 'zoomOutUp' ,'delay-0.3s');
+    document.getElementById("social-bar").classList.remove('animated', 'zoomOutRight' ,'delay-0.5s');
+    document.getElementById("social-bar").classList.add('animated', 'zoomIn' ,'delay-0.3s');
+    document.getElementById("conMenuBtn").classList.add('animated', 'zoomIn' ,'delay-0.3s');
+  } else {
+    document.getElementById("conMenuBtn").classList.add('animated', 'zoomOutUp' ,'delay-0.3s');
     document.getElementById("social-bar").classList.add('animated', 'zoomOutRight' ,'delay-0.5s');
   }
   prevScrollpos = currentScrollPos;
